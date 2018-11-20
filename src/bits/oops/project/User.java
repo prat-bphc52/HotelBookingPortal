4b1306a5e627ca535961c1f2f0bf831489d68a44
shreya <f20170093@hyderabad.bits-pilani.ac.in>
2018-11-20 16:48:02 +0530

    user class

---
+++ src/bits/oops/project/User.java
@@ -0,0 +1,34 @@
+package bits.oops.project;
+
+public class User {
+    String Name;
+    String Emailid;
+    String Username;
+    String password;
+    String Contact;
+    String Address;
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public String getContact() {
+        return Contact;
+    }
+
+    public String getEmailid() {
+        return Emailid;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+}
